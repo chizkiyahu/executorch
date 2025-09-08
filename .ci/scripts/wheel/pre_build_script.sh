732445fc121f8d53b066ebdaeb377e4c401bef92
Chizkiyahu Raful <chizkiyahuraful@gmail.com>
2025-09-09 02:12:49 +0300

    t3

--- .ci/scripts/wheel/pre_build_script.sh
+++ .ci/scripts/wheel/pre_build_script.sh
@@ -9,6 +9,20 @@ set -euxo pipefail
 
 # This script is run before building ExecuTorch binaries
 
+if [[ "$(uname -m)" == "aarch64" ]]; then
+  file="extension/llm/tokenizers/third-party/sentencepiece/src/CMakeLists.txt"
+
+  # Replace quoted or unquoted atomic with the literal ${ATOMIC_LIB}
+  # Note the single quotes around the sed script, so the shell won't expand ${...}.
+  sed -E 's/list\([[:space:]]*APPEND[[:space:]]+SPM_LIBS[[:space:]]+("?)(atomic)\1[[:space:]]*\)/list(APPEND SPM_LIBS \${ATOMIC_LIB})/g' \
+    "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+
+  grep -n 'list(APPEND SPM_LIBS ${ATOMIC_LIB})' "$file" && echo "the file $file has been modified for atomic to use full path"
+
+fi
+
+
+
 # Manually install build requirements because `python setup.py bdist_wheel` does
 # not install them. TODO(dbort): Switch to using `python -m build --wheel`,
 # which does install them. Though we'd need to disable build isolation to be
